995d2ab270810a0d64f96db0206a115a5451c2e7
reginamt03 <regina.madero.torres@gmail.com>
2020-04-14 15:51:53 -0500

    Controladores y Modelos

---
+++ ProyectoBaseDatos/Controllers/FichaMedicaController.cs
@@ -0,0 +1,41 @@
+﻿using ProyectoBaseDatos.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ProyectoBaseDatos.Controllers
+{
+    public class FichaMedicaController : ApiController
+    {
+        List<FichaMedica> fichaMed = new List<FichaMedica>();
+        // GET: api/FichaMedica
+        public List<FichaMedica> Get()
+        {
+            return fichaMed;
+        }
+
+        // GET: api/FichaMedica/5
+        public FichaMedica Get(int id)
+        {
+            return fichaMed.Where(x => x.IdPaciente== id).FirstOrDefault();
+        }
+
+        // POST: api/FichaMedica
+        public void Post(FichaMedica value)
+        {
+        }
+
+        // PUT: api/FichaMedica/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/FichaMedica/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ ProyectoBaseDatos/Models/FichaMedica.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoBaseDatos.Models
+{
+    public class FichaMedica
+    {
+        /// <summary>
+        /// Primary Key Identifier in SQL
+        /// </summary>
+        public int IdFichaMedica { get; set; }
+        /// <summary>
+        /// Foreing Key Paciente Identifier
+        /// </summary>
+        public int IdPaciente { get; set; }
+        /// <summary>
+        /// get's Nombre de Contacto Emergencia
+        /// </summary>
+        public string NombreContactoEmergencia { get; set; }
+        /// <summary>
+        /// get's Telefono Contacto de Emergencia
+        /// </summary>
+        public string TelContactoEmergencia { get; set; }
+        /// <summary>
+        /// get's alergias
+        /// </summary>
+        public string Alergias { get; set; }
+        /// <summary>
+        /// get's Enfermedades
+        /// </summary>
+        public string Enfermedades { get; set; }
+        /// <summary>
+        /// get's Medicamentos
+        /// </summary>
+        public string Medicamentos { get; set; }
+        /// <summary>
+        /// get's observaciones
+        /// </summary>
+        public string Obsevaciones { get; set; }
+    }
+}
