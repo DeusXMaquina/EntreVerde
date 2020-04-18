0de614cb3bfd04059832ba1effe96cafd472bd40
reginamt03 <regina.madero.torres@gmail.com>
2020-04-18 14:58:18 -0500

    Vista4 completa

---
+++ ProyectoBaseDatos/Models/Vista4.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoBaseDatos.Models
+{
+    public class Vista4
+    {
+        public string Terapeuta { get; set; }
+        public int Sesiones { get; set; }
+        public int TotalTerapias { get; set; }
+    }
+}
