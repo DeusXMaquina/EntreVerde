afc16c02458f52f1c673ae85327e6f59a3151bad
reginamt03 <regina.madero.torres@gmail.com>
2020-04-18 15:26:26 -0500

    Vista7 completa

---
+++ ProyectoBaseDatos/Models/Vista7.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoBaseDatos.Models
+{
+    public class Vista7
+    {
+        public string Enfermedad { get; set; }
+
+        public int NumeroPacientes { get; set; }
+    }
+}
