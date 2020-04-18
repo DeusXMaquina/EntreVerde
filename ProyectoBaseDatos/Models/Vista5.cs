bdd606fe7591e8bb9a95fb24beab9540bcff1d18
reginamt03 <regina.madero.torres@gmail.com>
2020-04-18 15:10:25 -0500

    Vista5 Completa

---
+++ ProyectoBaseDatos/Models/Vista5.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoBaseDatos.Models
+{
+    public class Vista5
+    {
+        public string Caballo { get; set; }
+        public int Sesiones { get; set; }
+        public int Terapias { get; set; }
+    }
+}
